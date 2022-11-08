3aee7cf9d5a5627a150e593b11812a8af90d0117
milosvujinovic <vujinovic.milos@gmail.com>
2022-11-08 16:06:43 +0100

    adding few lines of code

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const dice = document.querySelector('.dice')
+
+// Starting conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+dice.classList.add('hidden');
